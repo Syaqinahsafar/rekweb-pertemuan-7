91085af32681047960532ec22f19da8da104a183
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-10-22 04:58:44 +0800

    add new file and new function

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,8 +1,12 @@
 <?php
 
 class Home extends CI_Controller {
-    public function index()
+    public function index($name = '')
     {
-        echo 'home / index';
+        $data['title'] = 'Home Page';
+        $data['name'] = $name;
+        $this->load->view('templates/header', $data);
+        $this->load->view('home/index', $data);
+        $this->load->view('templates/footer');
     }
 }
