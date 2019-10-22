673e442c4ae393a2151b0c75fe6f0e5c222f9065
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-10-23 00:23:18 +0800

    modified some function

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,4 +1,15 @@
 <div class="container">
+    <?php if( $this->session->flashdata('flash') ) : ?>
+    <div class="row mt-3">
+        <div class="clo-md-6">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                Data mahasiswa <strong>success</strong> <?= $this->session->flashdata('flash'); ?>. <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
 
     <div class="row mt-3">
         <div class="col-md-6">
@@ -11,7 +22,10 @@
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach( $mahasiswa as $mhs ) : ?>
-                    <li class="list-group-item"><?= $mhs['name']; ?></li>
+                    <li class="list-group-item">
+                        <?= $mhs['name']; ?>
+                        <a href="<?= base_url(); ?>mahasiswa/delete/<?= $mhs['id']; ?>" class="badge badge-danger float-right" onclick="return confirm('confirm?')">delete</a>
+                    </li>
                 <?php endforeach; ?>
             </ul>
         </div>
