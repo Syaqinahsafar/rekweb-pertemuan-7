5cc7702e8ff2acfb6aa2c91c6f5d7a79ace331d4
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-10-22 22:34:12 +0800

    insert new function

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -2,6 +2,12 @@
 
     <div class="row mt-3">
         <div class="col-md-6">
+            <a href="<?= base_url(); ?>mahasiswa/add" class="btn btn-primary">Add Mahasiswa Data</a>
+        </div>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col-md-6">
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach( $mahasiswa as $mhs ) : ?>
