18fd6c9908435a4c6005a2a83404bccbce19d5f9
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-10-23 00:20:49 +0800

    inser function of delete and add

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -5,6 +5,7 @@ class Mahasiswa extends CI_Controller {
     {
         parent::__construct();
         $this->load->model('Mahasiswa_model');
+        $this->load->library('form_validation');
     }
 
 
@@ -21,8 +22,26 @@ class Mahasiswa extends CI_Controller {
     public function add()
     {
         $data['title'] = 'Add Mahasiswa Data Form';
-        $this->load->view('templates/header', $data);
-        $this->load->view('mahasiswa/add');
-        $this->load->view('templates/footer');
+
+        $this->form_validation->set_rules('name', 'Name', 'required');
+        $this->form_validation->set_rules('matric', 'Matric', 'required|numeric');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+        if( $this->form_validation->run() == FALSE ) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('mahasiswa/add');
+            $this->load->view('templates/footer');
+        } else {
+            $this->Mahasiswa_model->AddMahasiswaData();
+            $this->session->set_flashdata('flash', 'Added');
+            redirect('mahasiswa');
+        }
+    }
+
+    public function delete($id)
+    {
+        $this->Mahasiswa_model->deleteMahasiswaData($id);
+        $this->session->set_flashdata('flash', 'Deleted');
+        redirect('mahasiswa');
     }
 }
