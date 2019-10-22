99e5fdd79b758af9c112d7f2a9c625ca55fd0191
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-10-22 22:34:35 +0800

    add new file

---
+++ application/views/mahasiswa/add.php
@@ -0,0 +1,40 @@
+<div class="container">
+
+    <div class="row mt-3">
+        <div class="col-md-6">
+
+            <div class="card">
+                <div class="card-header">
+                    Add Mahasiswa Data Form
+                </div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name">
+                        </div>
+                        <div class="form-group">
+                            <label for="matric">Matric</label>
+                            <input type="text" class="form-control" id="matric">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control" id="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="course">Course</label>
+                            <select class="form-control" id="course" name="course">
+                            <option value="Artificial Intelligence">Artificial Intelligence</   option>
+                            <option value="Information Management">Information Management</option>
+                            <option value="Software Engineering">Software Engineering</option>
+                            <option value="Data Science">Data Science</option>
+                            <option value="Networking">Networking</option> 
+                            </select>
+                        </div>
+                        <button type="submit" name="add" class="btn btn-dark float-right">Add Data</button>
+                    </form> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
